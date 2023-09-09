a5cc7465ff5ffcb6db160c4a2435f6e7b563eaaf
BE21B017 <be21b017@smail.iitm.ac.in>
2023-09-09 17:59:51 +0530

    changes-5

--- src/components/composite/WorkspaceSidebar.tsx
+++ src/components/composite/WorkspaceSidebar.tsx
@@ -1,7 +1,5 @@
 import { Container, Box, Button } from "@mui/material";
-import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
 import {WorkspaceContext} from "@/providers/WorkspaceProvider";
 import {useContext} from "react";
 import { useWorkspaces } from "@/hooks/workspaces";
