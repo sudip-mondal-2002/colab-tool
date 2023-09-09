293f71866372bd6fda450ca57b762717ab11e39f
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2023-09-09 15:01:14 +0530

    auth pages

--- src/components/containers/SignIn.tsx
+++ src/components/containers/SignIn.tsx
@@ -1,26 +1,54 @@
 'use client'
 
+import {FormEvent, useState} from "react";
+import axios from "axios";
+import {Button, Container, TextField} from "@mui/material";
+
 export const SignIn = () => {
-
-  return (
-    <form action={"/api/user/signin"} method='POST'>
-      <label>
-        Email:
-        <input 
-          type="email"
-          name = "email"
-        />
-      </label>
-
-      <label>
-        Password: 
-        <input
-          type="password" 
-          name = "password"
-        />
-      </label>
-
-      <button type="submit">Sign In</button>
-    </form>
-  );
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSubmit = async () => {
+        await axios.post('/api/user/signin', {
+            email,
+            password
+        })
+        window.location.href = '/'
+    }
+    return (
+        <Container sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100vh'
+        }} >
+            <TextField
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                label="Email"
+                variant="outlined"
+                sx={{
+                    width: '40%',
+                    minWidth: '300px',
+                    marginY: '10px'
+                }}
+            />
+            <TextField
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                label="Password"
+                variant="outlined"
+                sx={{
+                    width: '40%',
+                    minWidth: '300px',
+                    marginY: '10px'
+                }}
+            />
+            <Button onClick={handleSubmit} variant={"contained"} sx={{
+                width: '40%',
+                minWidth: '300px',
+                marginY: '10px'
+            }}>Submit</Button>
+        </Container>
+    );
 }
