5cccc8c59ac567e251d8cfe9c901280014ea8035
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2023-09-09 16:04:55 +0530

    add providers

--- src/components/composite/WorkspaceSidebar.tsx
+++ src/components/composite/WorkspaceSidebar.tsx
@@ -2,18 +2,16 @@ import { Container, Box, Button } from "@mui/material";
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
+import {WorkspaceContext} from "@/providers/WorkspaceProvider";
+import {useContext} from "react";
 
-export const WorkspaceSidebar = ({workspaces}: {
-    workspaces: {
-        id: string,
-        name: string
-    }[]
-}) => {
+export const WorkspaceSidebar = () => {
+    const {workspaces} = useContext(WorkspaceContext)
     return (
         <>
           <Stack width={"29%"} height={"100%"}>
             {
-                workspaces.map((workspace) => {
+                workspaces.map((workspace:any) => {
                     return <Button key={workspace.id}>{workspace.name}</Button>
                 })
             }
