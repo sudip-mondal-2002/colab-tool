1e3153e0bc144ae6dab27297d465228bbf1b2333
BE21B017 <be21b017@smail.iitm.ac.in>
2023-09-09 14:39:26 +0530

    Signin and Signup

--- src/components/containers/SignIn.tsx
+++ src/components/containers/SignIn.tsx
@@ -1,4 +1,28 @@
 'use client'
+
+import { useState } from 'react';
+
 export const SignIn = () => {
-    return <></>
+
+  return (
+    <form action={"/api/user/signin"} method='POST'>
+      <label>
+        Email:
+        <input 
+          type="email"
+          name = "email"
+        />
+      </label>
+
+      <label>
+        Password: 
+        <input
+          type="password" 
+          name = "password"
+        />
+      </label>
+
+      <button type="submit">Sign In</button>
+    </form>
+  );
 }
--- src/components/containers/SignUp.tsx
+++ src/components/containers/SignUp.tsx
@@ -2,3 +2,62 @@
 export const SignUp = () => {
     return <></>
 }
+import { useState } from 'react';
+
+export const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email,
+          password
+        })  
+      });
+
+      const data = await response.json();
+      if(response.ok) {
+
+      } else {
+
+      }
+
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+
+      <button type="submit">Login</button>
+    </form>
+  );
+}
